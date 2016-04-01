fb53270570906a485303f9989288824fed69dddb
EliteMinerZMC <joshua.middleton@live.co.uk>
2016-04-01 12:10:30 +0100

    1.5 - User Registration

---
+++ app/src/main/java/com/joshuamiddletonfyp/myfitandroidfitnesspackage/Registration.java
@@ -0,0 +1,60 @@
+package com.joshuamiddletonfyp.myfitandroidfitnesspackage;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+
+public class Registration extends AppCompatActivity {
+
+    EditText firstName;
+    EditText lastName;
+    EditText age;
+    EditText email;
+    EditText username;
+    EditText password;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration);
+        firstName=(EditText)findViewById(R.id.register_first_name_edit);
+        lastName=(EditText)findViewById(R.id.register_last_name_edit);
+        age=(EditText)findViewById(R.id.register_agel_edit);
+        email=(EditText)findViewById(R.id.register_email_edit);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_registration, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public final static boolean isValidEmail(CharSequence target) {
+        if (target == null) {
+            return false;
+        } else {
+            return android.util.Patterns.EMAIL_ADDRESS.matcher(target).matches();
+        }
+    }
+}
